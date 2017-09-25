7bd753fa0add34db20917b9dd000868f39145568
alopezca <alopezca16dw@ikzubirimanteo.com>
2017-09-25 11:30:20 +0200

    Ejercicio 1 - Link hellow agur

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
+/*
+ * Route::get('/', function () {
     return view('welcome');
+});
+*/
+Route::get('/', function () {
+    return(view('bienvenido'));
+});
+Route::get('/agur', function () {
+    return(view('agur'));
 });
